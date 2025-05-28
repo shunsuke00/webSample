92bba4213fcde1ed72e5c3df2898863dafc3b7fd
shunshun <134624826+shunsuke00@users.noreply.github.com>
2025-05-28 22:05:11 +0900

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute("href").substring(1);
+        const targetElement = document.getElementById(targetId);
+        const offset = 40; // 調整したいピクセル数
+
+        if (targetElement) {
+            const top = targetElement.getBoundingClientRect().top + window.pageYOffset - offset;
+            window.scrollTo({ top, behavior: "smooth" });
+        }
+    });
+});
