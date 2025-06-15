a35acda4b1b2edf55593ae00ea70b9c160fdc2c7
土田駿介 <nemu.shun@icloud.com>
2025-06-15 21:12:05 +0900

    [UPDATE] add name option

--- ColorName.js
+++ ColorName.js
@@ -1,8 +1,17 @@
 ["first", "second", "third"].forEach(grade => {
-    let node = document.querySelector(`#box-${grade}`);
-    node.addEventListener("click", () => {
-        document.querySelectorAll(`.${grade}`).forEach(element => {
-            element.classList.toggle("invisible", !node.checked);
+    let node_grade = document.querySelector(`#box-${grade}`);
+    node_grade.addEventListener("click", () => {
+        document.querySelectorAll(`.${grade}`).forEach(element_grade => {
+            element_grade.classList.toggle("invisible_grade", !node_grade.checked);
+        });
+    });
+});
+
+["japanese-name", "english-name"].forEach(name => {
+    let node_name = document.querySelector(`#box-${name}`);
+    node_name.addEventListener("click", () => {
+        document.querySelectorAll(`.${name}`).forEach(element_name => {
+            element_name.classList.toggle("invisible_name", !node_name.checked);
         });
     });
 });
